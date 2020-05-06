e30f3715a4506dd356452816527f14ac08a9ae94
TaBayram <TaBayram@users.noreply.github.com>
2020-05-06 17:10:41 +0300

    v00.00.01
    Stylelar eklendi

--- anaSayfa.php
+++ anaSayfa.php
@@ -2,62 +2,22 @@
 
 <html>
     <head>
-   
+        <link rel="stylesheet" type="text/css" href="siteStyle.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width = device-width, initial-scale =1.">
 
-        <style>
-        .deneme{
-            width:700px;
-            height:500px;
-            border: 6px solid #FF0000;
-            margin:20px auto;
-        }
-
-        .deneme1{
-            width:304px;
-            height:300px;
-            border: 3px solid #FF0000;
-            margin:30px 20px;
-            float:left;
-        }
-
-        .deneme2{
-            width:304px;
-            height:300px;
-            border:3px solid #FF0000;
-            margin:30px 20px;
-            float:right;
-        }
-        </style>
-
 </head>
 <body>
-    <div class="deneme">
+    <style>
+    body {
+        background-image: linear-gradient(to bottom right, #12567A, #5e87c1);
+    }
+    </style>
+    <?php include 'header.php';?>
+    <div class="mainCanvas">
         <div class="deneme1"></div>
         <div class="deneme2"></div>
     </div>
-
-    <?php
-        $con = new PDO("mysql:host=localhost;dbname=attb_db","pma","");
-        if(isset($_POST['btn'])){
-            $name = $_FILES[$_POST['myFile']]['name'];
-            $type = $_FILES[$_POST['myFile']]['type'];
-            $data = file_get_contents($_FILES[$_POST['myFile']]['tmp_name']);
-            $stmt = $dbh->prepare("insert into films values('','','','','','','','','',?,'')");
-            $stmt->bindParam(1,$data);
-            $stmt->execute();
-        }
-
-    ?>
-
-
-
-
-    <form enctype="multipart/form-data" method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-    <input type="file" name="myfile" /> 
-    <button name="btn" value="Upload">Yukle</button>
-    </form>
 
    
 </body>
