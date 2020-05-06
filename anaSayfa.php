c5130163cfdc3dc6762734d74c1660123b5e69ab
abTURGUT <48166225+abTURGUT@users.noreply.github.com>
2020-05-06 13:56:16 +0300

    Initial commit

---
+++ anaSayfa.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+   
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width = device-width, initial-scale =1.">
+
+        <style>
+        .deneme{
+            width:700px;
+            height:500px;
+            border: 6px solid #FF0000;
+            margin:20px auto;
+        }
+
+        .deneme1{
+            width:304px;
+            height:100px;
+            border: 3px solid #FF0000;
+            margin:30px 20px;
+            float:left;
+        }
+
+        .deneme2{
+            width:304px;
+            height:100px;
+            border:3px solid #FF0000;
+            margin:30px 20px;
+            float:right;
+        }
+        </style>
+
+</head>
+<body>
+    <div class="deneme">
+        <div class="deneme1"></div>
+        <div class="deneme2"></div>
+    </div>
+   
+</body>
+</html>
