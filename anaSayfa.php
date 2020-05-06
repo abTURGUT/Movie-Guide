a422259fdfe13efb841a813442a16e69c0623f43
abTURGUT <48166225+abTURGUT@users.noreply.github.com>
2020-05-06 15:50:28 +0300

    abc
    bi şey yok soryy

--- anaSayfa.php
+++ anaSayfa.php
@@ -16,7 +16,7 @@
 
         .deneme1{
             width:304px;
-            height:100px;
+            height:300px;
             border: 3px solid #FF0000;
             margin:30px 20px;
             float:left;
@@ -24,7 +24,7 @@
 
         .deneme2{
             width:304px;
-            height:100px;
+            height:300px;
             border:3px solid #FF0000;
             margin:30px 20px;
             float:right;
@@ -37,6 +37,28 @@
         <div class="deneme1"></div>
         <div class="deneme2"></div>
     </div>
+
+    <?php
+        $con = new PDO("mysql:host=localhost;dbname=attb_db","pma","");
+        if(isset($_POST['btn'])){
+            $name = $_FILES[$_POST['myFile']]['name'];
+            $type = $_FILES[$_POST['myFile']]['type'];
+            $data = file_get_contents($_FILES[$_POST['myFile']]['tmp_name']);
+            $stmt = $dbh->prepare("insert into films values('','','','','','','','','',?,'')");
+            $stmt->bindParam(1,$data);
+            $stmt->execute();
+        }
+
+    ?>
+
+
+
+
+    <form enctype="multipart/form-data" method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+    <input type="file" name="myfile" /> 
+    <button name="btn" value="Upload">Yukle</button>
+    </form>
+
    
 </body>
 </html>
